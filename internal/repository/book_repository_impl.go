21df213c974929fea2839c32219d081d2819ad7f
Elisio Augusto <elisiouiux@gmail.com>
2026-03-19 19:58:15 +0100

    book impl.go

--- internal/repository/book_repository_impl.go
+++ internal/repository/book_repository_impl.go
@@ -24,6 +24,10 @@ func (r *GormBookRepository) FindAll() ([]models.Book, error) {
 	return books, nil
 }
 
+func (r *GormBookRepository) Create(book *models.Book) error {
+	return r.db.Create(book).Error
+}
+
 func (r *GormBookRepository) GetByID(id string) (*models.Book, error) {
 	var book models.Book
 	err := r.db.First(&book, "id = ?", id).Error
@@ -32,4 +36,8 @@ func (r *GormBookRepository) GetByID(id string) (*models.Book, error) {
 
 func (r *GormBookRepository) Update(book *models.Book) error {
 	return r.db.Save(book).Error
+}
+
+func (r *GormBookRepository) Delete(id string) error {
+	return r.db.Delete(&models.Book{}, id).Error
 }
